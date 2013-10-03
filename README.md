1f5c5e8ee82fca422390000f5c430149dd10c566
rkaneko <kanek.o.dev@gmail.com>
2013-10-03 18:45:28 +0900

    added BM method.

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Samples
 
 KMP (Knuth-Morris-Pratt)
 
-+ on REPL
++ on sbt console 
 
         scala> import com.rkaneko.algorithm.search.KMP
         import com.rkaneko.algorithm.search.KMP
@@ -25,3 +25,31 @@ KMP (Knuth-Morris-Pratt)
 
         scala> KMP.search("I'm Bob .", "")
         res3: Boolean = true
+
+
+BM (Boyer-Moore)
+
++ on sbt console
+
+        scala> import com.rkaneko.algorithm.search.BM
+        import com.rkaneko.algorithm.search.BM
+
+        scala> val text = "djfkjalkgjsabcd"
+        text: String = djfkjalkgjsabcd
+
+        scala> val pattern = "abcd"
+        pattern: String = abcd
+
+        scala> implicit val isDebug = true
+        isDebug: Boolean = true
+
+        scala> BM.search(text, pattern)
+        k equals d ?
+        k equals d ?
+        a equals d ?
+        d equals d ?
+        c equals c ?
+        b equals b ?
+        a equals a ?
+        found abcd from text(11) !
+        res0: Boolean = true
---
+++ src/main/scala/BM.scala
@@ -0,0 +1,76 @@
+package com.rkaneko.algorithm.search
+
+/**
+ * Indexes class .
+ *
+ * @param patternIndex Index of pattern text .
+ * @param textIndex Index of text text .
+ * @param tailIndex Index of text tail .
+ */
+private case class BMIndex(
+  patternIndex: Int,
+  textIndex: Int,
+  tailIndex: Int)
+
+
+/**
+ * BM (Boyer-Moore) method .
+ */
+object BM {
+
+  /**
+   * Search for pattern from text .
+   *
+   * @param text The text you want to search for pattern from .
+   * @param pattern The text you want to search for from text .
+   * @param isDebug If you want print process of searching, you may well define 'implicit val isDebug = true' .
+   * @return The pattern exists in text or not .
+   */
+  def search(text: String, pattern: String)(implicit isDebug: Boolean = false): Boolean = {
+    val textLength = text.length
+    val patternLength = pattern.length
+    val table = makeTable(pattern)
+    def inner(patternIndex: Int, textIndex: Int, tailIndex: Int): Boolean = {
+      if (textIndex >= textLength) false
+      else {
+        if (patternIndex < 0) {
+          if (isDebug)
+            println(s"""found ${pattern} from text(${tailIndex - patternLength + 1}) !""")
+          true
+        } else {
+          if (isDebug)
+            println(s"""${text(textIndex)} equals ${pattern(patternIndex)} ?""")
+          val next = nextIndex(patternIndex, textIndex, tailIndex)
+          inner(next.patternIndex, next.textIndex, next.tailIndex)
+        }
+      }
+    }
+    def nextIndex(patternIndex: Int, textIndex: Int, tailIndex: Int): BMIndex = {
+      val charOfText = text(textIndex)
+      if (charOfText == pattern(patternIndex))
+        BMIndex(patternIndex = patternIndex - 1, textIndex = textIndex - 1, tailIndex = tailIndex)
+      else {
+        val nextPatternIndex = patternLength - 1
+        table.get(charOfText) match {
+          case None => BMIndex(nextPatternIndex, tailIndex + patternLength, tailIndex + patternLength)
+          case Some(shiftFromTail) =>
+            if ((patternLength - shiftFromTail - 1) >= patternLength)
+              BMIndex(nextPatternIndex, tailIndex + 1, tailIndex + 1)
+            else
+              BMIndex(nextPatternIndex, tailIndex + shiftFromTail, tailIndex + shiftFromTail)
+        }
+      }
+    }
+    inner(patternLength - 1, patternLength - 1, patternLength - 1)
+  }
+
+  /**
+   * Make table for BM method .
+   *
+   * @param pattern The text which you want to search for .
+   * @return Table for BM method .
+   */
+  private def makeTable(pattern: String): Map[Char, Int] = {
+    pattern.reverse.toList.zipWithIndex.reverse.toMap
+  }
+}
